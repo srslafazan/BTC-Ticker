a83178c1468ed1a2d5325ac93ac9afc0ecd6aed9
Justin Sisley <sisley.justin@gmail.com>
2017-12-24 11:34:58 -0800

    Project cleanup

--- main.js
+++ main.js
@@ -1,49 +1,50 @@
-const {app, BrowserWindow, ipcMain, Tray} = require('electron')
-const path = require('path')
+const { app, BrowserWindow, ipcMain, Tray } = require('electron');
+const path = require('path');
 
-const assetsDirectory = path.join(__dirname, 'assets')
+const assetsDirectory = path.join(__dirname, 'assets');
 
-let tray = undefined
-let window = undefined
+let tray = undefined;
+let window = undefined;
 
 // Don't show the app in the doc
 app.dock.hide()
 
 app.on('ready', () => {
-  createTray()
-  createWindow()
+  createTray();
+  createWindow();
 })
 
 // Quit the app when the window is closed
 app.on('window-all-closed', () => {
-  app.quit()
+  app.quit();
 })
 
 const createTray = () => {
-  tray = new Tray(path.join(assetsDirectory, 'xrp.png'))
-  tray.on('right-click', toggleWindow)
-  tray.on('double-click', toggleWindow)
-  tray.on('click', function (event) {
-    toggleWindow()
+  tray = new Tray(path.join(assetsDirectory, 'xrp.png'));
+  tray.on('right-click', toggleWindow);
+  tray.on('double-click', toggleWindow);
+
+  tray.on('click', function(event) {
+    toggleWindow();
 
     // Show devtools when command clicked
     if (window.isVisible() && process.defaultApp && event.metaKey) {
-      window.openDevTools({mode:'detach'})
+      window.openDevTools({ mode:'detach' });
     }
   })
 }
 
 const getWindowPosition = () => {
-  const windowBounds = window.getBounds()
-  const trayBounds = tray.getBounds()
+  const windowBounds = window.getBounds();
+  const trayBounds = tray.getBounds();
 
   // Center window horizontally below the tray icon
-  const x = Math.round(trayBounds.x + (trayBounds.width / 2) - (windowBounds.width / 2))
+  const x = Math.round(trayBounds.x + (trayBounds.width / 2) - (windowBounds.width / 2));
 
   // Position window 4 pixels vertically below the tray icon
-  const y = Math.round(trayBounds.y + trayBounds.height + 4)
+  const y = Math.round(trayBounds.y + trayBounds.height + 4);
 
-  return {x: x, y: y}
+  return { x, y };
 }
 
 const createWindow = () => {
@@ -60,37 +61,41 @@ const createWindow = () => {
       // hidden
       backgroundThrottling: false
     }
-  })
-  window.loadURL(`file://${path.join(__dirname, 'index.html')}`)
+  });
+
+  window.loadURL(`file://${path.join(__dirname, 'index.html')}`);
 
   // Hide the window when it loses focus
   window.on('blur', () => {
     if (!window.webContents.isDevToolsOpened()) {
-      window.hide()
+      window.hide();
     }
-  })
+  });
 }
 
 const toggleWindow = () => {
   if (window.isVisible()) {
-    window.hide()
+    window.hide();
   } else {
-    showWindow()
+    showWindow();
   }
-}
+};
 
 const showWindow = () => {
-  const position = getWindowPosition()
-  window.setPosition(position.x, position.y, false)
-  window.show()
-  window.focus()
+  const position = getWindowPosition();
+
+  window.setPosition(position.x, position.y, false);
+  window.show();
+  window.focus();
 }
 
 ipcMain.on('show-window', () => {
-  showWindow()
+  showWindow();
 })
 
 ipcMain.on('data-updated', (event, data) => {
-  tray.setTitle(`$${data.USD}`)
-  tray.setToolTip(`$${data.USD}`)
+  const value = `$${data.USD}`;
+
+  tray.setTitle(value);
+  tray.setToolTip(value);
 })
---
+++ readme.md
@@ -0,0 +1,45 @@
+<p align="center">
+  <img alt="XRP" src="https://cdn.coinranking.com/Bkuz9Hd_-/xrp.svg" width="144">
+</p>
+
+<h3 align="center">
+  XRP Ticker
+</h3>
+
+<p align="center">
+  A simple Mac OS X tray app that displays the current price of XRP in USD
+</p>
+
+<p align="center">
+  <a href="https://github.com/justinsisley/XRP-Ticker/blob/master/license"><img src="https://img.shields.io/badge/license-MIT-blue.svg?style=flat"></a>
+  <a href="https://github.com/justinsisley/XRP-Ticker/releases/latest"><img src="https://img.shields.io/github/release/justinsisley/XRP-Ticker.svg" /></a>
+  <a href="https://david-dm.org/justinsisley/XRP-Ticker"><img src="https://david-dm.org/justinsisley/XRP-Ticker/status.svg?style=flat" ></a>
+  <a href="https://david-dm.org/justinsisley/XRP-Ticker?type=dev"><img src="https://david-dm.org/justinsisley/XRP-Ticker/dev-status.svg?style=flat"></a>
+</p>
+
+## Installation
+
+```bash
+git clone https://github.com/justinsisley/XRP-Ticker
+cd XRP-Ticker
+npm install
+```
+
+## Development
+
+```bash
+npm start
+```
+
+## Packaging
+
+```bash
+npm run package && open dist
+```
+
+## Credits
+
+- Depends on [Electron](http://electron.atom.io)
+- Depends on [photon](http://photonkit.com)
+- Uses the [CryptoCompare API](https://www.cryptocompare.com/)
+- Icon from [Coinranking](https://coinranking.com/)
